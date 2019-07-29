2e61e39dea328431b8ded3256bad47302855c1c9
Stefan Bethge <stefan+github@lanpartei.de>
2019-07-29 13:46:44 +0200

    a few fixes
    * compare website text instead of html directly
    * use config file, output RSS for CGI usage

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # url-checker
-Simple script to check for modifications of URLs and output RSS to file
+Simple script to check for modifications of URLs and output RSS of updated urls.
+
+needs python3, python3-html2text, python3-PyRSS2Gen, python3-yaml
+
+Intended to run as CGI script and can be called from RSS reader.
+
+Set URLs to monitor in ~/.url-checker.yaml
+Cached URL content is saved to ~/.cache/url-checker/
--- url-checker.py
+++ url-checker.py
@@ -1,13 +1,11 @@
-#!/opt/local/bin/python3.6
+#!/usr/bin/env python3
 
 import urllib.request
+import html2text
 import re
 import PyRSS2Gen
 import datetime
-
-urls = (
-        'https://zim-wiki.org/downloads/',
-        )
+import yaml
 
 def slugify(value):
     """
@@ -16,34 +14,49 @@ def slugify(value):
     and converts spaces to hyphens.
     """
     import unicodedata
-    #value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
     value = unicodedata.normalize('NFKC', value)
     value = re.sub(r'[^\w\s-]', '', value).strip().lower()
     return re.sub(r'[-\s]+', '-', value)
 
 if __name__ == "__main__":
     from pathlib import Path
+    import os
+
     home = Path.home()
+
+    with open(home / '.url-checker.yaml', 'r') as stream:
+        try:
+            config = yaml.load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
     cache_path = home / ".cache" / "url-checker"
+    if not os.path.exists(cache_path):
+       os.makedirs(cache_path)
 
     updated_urls = []
-    for url in urls:
-        contents = str(urllib.request.urlopen(url).read())
+    for url in config['urls']:
+        contents = html2text.html2text(str(urllib.request.urlopen(url).read()))
 
-        with open(cache_path / "{}.txt".format(slugify(url)), "a+") as f:
-            f.seek(0)
-            old_contents = f.read()
-            if (old_contents != contents):
-                f.seek(0)
+        try:
+            with open(cache_path / "{}.txt".format(slugify(url)), "r") as f:
+                old_contents = f.read()
+                f.close()
+        except FileNotFoundError:
+            old_contents = ""
+
+        if (old_contents != contents):
+            with open(cache_path / "{}.txt".format(slugify(url)), "w") as f:
+                #f.seek(0)
                 f.write(contents)
-                f.truncate()
+                #f.truncate()
 
-                print("url {} changed".format(url))
+                #print("url {} changed".format(url))
                 updated_urls.append(url)
-            f.close()
 
+    updated_html = ""
     for url in updated_urls:
-        updated_html = "<ul>"
+        updated_html += "<ul>"
         updated_html += '<li><a href="{}">{}</a></li>'.format(url, url)
         updated_html += "</ul>"
 
@@ -61,5 +74,14 @@ if __name__ == "__main__":
             )]
         )
 
-        rss.write_xml(open("url-checker-feed.xml", "w"))
-
+        rss.write_xml(open(cache_path / "url-checker-feed.xml", "w"))
+        print("Content-Type: text/xml; charset=utf-8\n")
+        print(rss.to_xml())
+    else:
+        #output previous rss file
+        with open(cache_path / "url-checker-feed.xml", "r") as f:
+            contents = f.read()
+            print("Content-Type: text/xml; charset=utf-8\n")
+            print(str(contents))
+            f.close()
+       
